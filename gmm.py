3d1662ad30c18f2a756b76f83d6836686f138df5
Igor Mikushkin <igor.mikushkin@gmail.com>
2019-05-27 23:43:40 +0400

    simplification

--- gmm.py
+++ gmm.py
@@ -4,39 +4,34 @@ import lxml.etree as T
 import sys
 import lxml.builder
 import re
+import collections as C
 
 E = lxml.builder.ElementMaker()
 
-namespaces = {'x': 'http://www.opengis.net/kml/2.2'}
+ns = {'x': 'http://www.opengis.net/kml/2.2'}
+google_style_regex = re.compile(r'^#icon-\d{4}-([0-9A-F]{6})')
 
-mapsMeStyles = {
-    'placemark-red':    'http://maps.me/placemarks/placemark-red.png',
-    'placemark-blue':   'http://maps.me/placemarks/placemark-blue.png',
-    'placemark-purple': 'http://maps.me/placemarks/placemark-purple.png',
-    'placemark-yellow': 'http://maps.me/placemarks/placemark-yellow.png',
-    'placemark-pink':   'http://maps.me/placemarks/placemark-pink.png',
-    'placemark-brown':  'http://maps.me/placemarks/placemark-brown.png',
-    'placemark-green':  'http://maps.me/placemarks/placemark-green.png',
-    'placemark-orange': 'http://maps.me/placemarks/placemark-orange.png'
+style_map = {
+    'C2185B': 'placemark-red',
+    '0288D1': 'placemark-blue',
+    '673AB7': 'placemark-blue',
+    '880E4F': 'placemark-purple',
+    '9C27B0': 'placemark-purple',
+    'FBC02D': 'placemark-yellow',
+    'FFD600': 'placemark-yellow',
+    'FF5252': 'placemark-pink',
+    '817717': 'placemark-brown',
+    '097138': 'placemark-green',
+    '558B2F': 'placemark-green',
+    '7CB342': 'placemark-green',
+    'E65100': 'placemark-orange',
+    'F57C00': 'placemark-orange',
+    'F9A825': 'placemark-orange',
 }
 
-styleMap = {
-    '0288D1': '#placemark-blue',
-    '097138': '#placemark-green',
-    '558B2F': '#placemark-green',
-    '673AB7': '#placemark-blue',
-    '7CB342': '#placemark-green',
-    '817717': '#placemark-brown',
-    '880E4F': '#placemark-purple',
-    '9C27B0': '#placemark-purple',
-    'C2185B': '#placemark-red',
-    'E65100': '#placemark-orange',
-    'F57C00': '#placemark-orange',
-    'F9A825': '#placemark-orange',
-    'FBC02D': '#placemark-yellow',
-    'FF5252': '#placemark-pink',
-    'FFD600': '#placemark-yellow',
-}
+
+def unique_styles():
+    return C.OrderedDict(zip(style_map.values(), [None] * len(style_map)))
 
 
 def indent(elem, level=0):
@@ -47,7 +42,7 @@ def indent(elem, level=0):
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for elem in elem:
-            indent(elem, level+1)
+            indent(elem, level + 1)
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
     else:
@@ -55,29 +50,31 @@ def indent(elem, level=0):
             elem.tail = i
 
 
-rex = re.compile('^#icon-\d{4}-([0-9A-F]{6})')
-def mapsMeStyle(style):
-    m = rex.match(style)
-    if m and m.group(1) in styleMap:
-        return styleMap[m.group(1)]
+def maps_me_style(style):
+    m = google_style_regex.match(style)
+    if m and m.group(1) in style_map:
+        return '#' + style_map[m.group(1)]
     return style
 
 
-def process(filename):
+def process(doc):
+    for i in doc.xpath('//x:Style | //x:StyleMap', namespaces=ns):
+        i.getparent().remove(i)
+    for i in doc.xpath('//x:Folder//x:Placemark//x:styleUrl', namespaces=ns):
+        i.text = maps_me_style(i.text)
+    for i, name in enumerate(unique_styles()):
+        ref = f'http://maps.me/placemarks/{name}.png'
+        style = E.Style(E.IconStyle(E.Icon(E.href(ref))), id=name)
+        doc.insert(i, style)
+
+
+def main(filename):
     with open(filename, 'r') as f:
         root = T.parse(f)
-        doc = root.find('x:Document', namespaces=namespaces)
+        doc = root.find('x:Document', ns)
         if doc is None:
             return
-        for elem in doc.xpath('//x:Style | //x:StyleMap', namespaces=namespaces):
-            elem.getparent().remove(elem)
-        for elem in doc.xpath('//x:Folder//x:Placemark//x:styleUrl', namespaces=namespaces):
-            elem.text = mapsMeStyle(elem.text)
-        pos = 0
-        for name, ref in mapsMeStyles.items():
-            style = E.Style(E.IconStyle(E.Icon(E.href(ref))), id=name)
-            doc.insert(pos, style)
-            pos += 1
+        process(doc)
         indent(root.getroot())
         string = T.tostring(root,
                             pretty_print=True,
@@ -85,8 +82,9 @@ def process(filename):
                             xml_declaration=True)
         sys.stdout.buffer.write(string)
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('usage: gmm <kml>')
         sys.exit(1)
-    process(sys.argv[1])
+    main(sys.argv[1])
